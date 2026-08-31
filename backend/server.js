7b43a8a44d4cf4fc2e68124085a94ecc898fd598
Janvi Singh <janvi.singh1209@gmail.com>
2026-08-31 17:03:29 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const axios = require("axios"); // ✅ to call Flask API
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
